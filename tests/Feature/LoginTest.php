7fcc38bb7702161403086f7084a23dfb9c6bcefc
wiryadev <abrarwiryawan@gmail.com>
2022-02-23 13:37:53 +0700

    add login test for success case

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -15,8 +17,101 @@ class LoginTest extends TestCase
      */
     public function test_login_success()
     {
-        $response = $this->get('/');
+        $this->seed(UserSeeder::class);
 
-        $response->assertStatus(200);
+        $response = $this
+            ->withHeaders([
+                'Accept' => "application/json"
+            ])
+            ->post('/login', [
+                'email' => "admin@transtory.com",
+                'password' => env('DEFAULT_ADMIN_PASSWORD'),
+            ]);
+
+        $response->assertOk();
+    }
+
+    public function test_login_success_json()
+    {
+        $this->seed(UserSeeder::class);
+
+        $response = $this
+            ->withHeaders([
+                'Accept' => "application/json"
+            ])
+            ->postJson('/api/login', [
+                'email' => "admin@transtory.com",
+                'password' => env('DEFAULT_ADMIN_PASSWORD'),
+            ]);
+
+        $response->assertOk();
+    }
+
+    public function test_login_success_json_path()
+    {
+        $this->seed(UserSeeder::class);
+
+        $response = $this
+            ->withHeaders([
+                'Accept' => "application/json"
+            ])
+            ->postJson('/api/login', [
+                'email' => "admin@transtory.com",
+                'password' => env('DEFAULT_ADMIN_PASSWORD'),
+            ]);
+
+        $response
+            ->assertJsonPath('data.user.name', "Admin")
+            ->assertJsonPath('data.user.email', "admin@transtory.com");
+    }
+
+    public function test_login_success_json_response()
+    {
+        $this->seed(UserSeeder::class);
+
+        $response = $this
+            ->withHeaders([
+                'Accept' => "application/json"
+            ])
+            ->postJson('/api/login', [
+                'email' => "admin@transtory.com",
+                'password' => env('DEFAULT_ADMIN_PASSWORD'),
+            ]);
+
+        $response
+            ->assertJson([
+                'data' => [
+                    'user' => [
+                        'name' => "Admin",
+                        'email' => "admin@transtory.com"
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * Check if Token exists and exactly 42 in length
+     */
+    public function test_login_success_json_token()
+    {
+        $this->seed(UserSeeder::class);
+
+        $response = $this
+            ->withHeaders([
+                'Accept' => "application/json"
+            ])
+            ->postJson('/api/login', [
+                'email' => "admin@transtory.com",
+                'password' => env('DEFAULT_ADMIN_PASSWORD'),
+            ]);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'access_token',
+                'token_type'
+            ]
+        ]);
+
+        $this->assertTrue(strlen($response['data']['access_token']) == 42);
     }
 }
