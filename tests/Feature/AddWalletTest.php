0d8c08ed91a6fbdbcf238ffe968206845f3a30a3
wiryadev <abrarwiryawan@gmail.com>
2022-03-01 19:13:02 +0700

    add failed empty bank id case

--- tests/Feature/AddWalletTest.php
+++ tests/Feature/AddWalletTest.php
@@ -14,7 +14,7 @@ class AddWalletTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_add_wallet_success()
     {
         $this->seed();
         $accountNumber = "01157690897";
@@ -50,6 +50,41 @@ class AddWalletTest extends TestCase
                         'banks_id' => "1",
                         'account_no' => $accountNumber
                     ]
+                ]
+            ]);
+    }
+
+    public function test_add_wallet_failed_bank_id()
+    {
+        $this->seed();
+        $accountNumber = "01157690897";
+
+        $loginResponse = $this
+            ->withHeaders([
+                'Accept' => "application/json"
+            ])
+            ->postJson('/api/login', [
+                'email' => "admin@transtory.com",
+                'password' => env('DEFAULT_ADMIN_PASSWORD'),
+            ]);
+
+        $token = $loginResponse['data']['access_token'];
+
+        $response = $this
+            ->withHeaders([
+                'Accept' => "application/json",
+                'Authorization' => "Bearer $token",
+            ])
+            ->postJson('/api/wallet/add', [
+                'account_no' => $accountNumber,
+            ]);
+
+        $response
+            ->assertUnprocessable()
+            ->assertJsonPath('data.message', "The banks id field is required.")
+            ->assertJson([
+                'data' => [
+                    'message' => "The banks id field is required."
                 ]
             ]);
     }
